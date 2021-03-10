257bd085f7b90e78fdb2c63c87d3387d794e3e5e
EugeneShab <dev.eugene.shab@gmail.com>
2021-03-10 21:37:10 +0200

    Refactoring

--- Block/Customer/Widget/AbstractAddress.php
+++ Block/Customer/Widget/AbstractAddress.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Swarming\SubscribePro\Block\Customer\Widget;
 
+use Magento\Customer\Api\AddressRepositoryInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Template;
@@ -11,7 +12,7 @@ use Magento\Store\Model\ScopeInterface;
 use Swarming\SubscribePro\Platform\Manager\Customer as PlatformCustomer;
 use Swarming\SubscribePro\Platform\Tool\Oauth as PlatformOauthTool;
 
-class Address extends Template
+abstract class AbstractAddress extends Template
 {
     /**
      * @var PlatformCustomer
@@ -25,18 +26,24 @@ class Address extends Template
      * @var PlatformOauthTool
      */
     protected $oauthTool;
+    /**
+     * @var AddressRepositoryInterface
+     */
+    protected $addressRepository;
 
     public function __construct(
         Context $context,
         PlatformCustomer $platformCustomer,
         CustomerSession $customerSession,
         PlatformOauthTool $oauth,
+        AddressRepositoryInterface $addressRepository,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->platformCustomerManager = $platformCustomer;
         $this->customerSession = $customerSession;
         $this->oauthTool = $oauth;
+        $this->addressRepository = $addressRepository;
     }
 
     public function isAddressBookEnabled(): bool
@@ -51,47 +58,61 @@ class Address extends Template
         );
     }
 
-    public function getAddressWidgetUrl()
+    public function getAddressWidgetUrl(): string
     {
         $store = $this->_storeManager->getStore();
         $path = 'swarming_subscribepro/advanced/hosted_address_book_widget_url';
-        return $this->_scopeConfig->getValue(
+        $value = $this->_scopeConfig->getValue(
             $path,
             ScopeInterface::SCOPE_STORE,
             $store
         );
+
+        return ($value)?? '';
     }
 
-    public function getAddressWidgetJson()
+    public function getAddressWidgetJson(): string
     {
         $store = $this->_storeManager->getStore();
         $path = 'swarming_subscribepro/advanced/custom_json_hosted_address_book_widget';
-        return $this->_scopeConfig->getValue(
+        $value = $this->_scopeConfig->getValue(
             $path,
             ScopeInterface::SCOPE_STORE,
             $store
         );
+
+        return ($value)?? '';
     }
 
-    public function getPlatformCustomerId()
+    public function getPlatformCustomerId(): ?int
     {
         try {
+            $customerId = $this->getCustomerId();
+            if (!$customerId) {
+                return null;
+            }
             return $this->platformCustomerManager->getCustomerById(
-                $this->getCustomerId(),
+                $customerId,
                 false,
                 $this->customerSession->getCustomer()->getWebsiteId()
             )->getId();
         } catch (NoSuchEntityException $e) {
-            return false;
+            $this->_logger->critical($e->getMessage());
+            return null;
         }
     }
 
     /**
-     * @return array
+     * @return string
      */
     public function getWidgetAccessTokenForCurrentCustomer(): string
     {
+        $platformCustomerId = $this->getPlatformCustomerId();
+        if (!$platformCustomerId) {
+            return '';
+        }
         $accessToken = $this->oauthTool->getWidgetAccessTokenByCustomerId($this->getPlatformCustomerId());
+
         return $accessToken['access_token'];
     }
 
